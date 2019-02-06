fd75c635d3dfe5e07b440c6b76007c7152829578
Johannes Schulte <JohannesB.Schulte@gmail.com>
2019-02-06 18:21:01 +0100

    further cleanup

--- src/main/java/de/rwth/i2/attestor/grammar/confluence/CriticalPairs.java
+++ src/main/java/de/rwth/i2/attestor/grammar/confluence/CriticalPairs.java
@@ -2,13 +2,11 @@ package de.rwth.i2.attestor.grammar.confluence;
 
 import de.rwth.i2.attestor.grammar.CollapsedHeapConfiguration;
 import de.rwth.i2.attestor.grammar.Grammar;
+import de.rwth.i2.attestor.grammar.confluence.jointMorphism.GraphElement;
 import de.rwth.i2.attestor.graph.Nonterminal;
 import de.rwth.i2.attestor.graph.SelectorLabel;
 import de.rwth.i2.attestor.graph.digraph.NodeLabel;
 import de.rwth.i2.attestor.graph.heap.HeapConfiguration;
-import de.rwth.i2.attestor.graph.heap.HeapConfigurationBuilder;
-import de.rwth.i2.attestor.graph.heap.internal.InternalHeapConfiguration;
-import de.rwth.i2.attestor.graph.heap.internal.InternalHeapConfigurationBuilder;
 import de.rwth.i2.attestor.graph.morphism.Graph;
 import de.rwth.i2.attestor.types.Type;
 import de.rwth.i2.attestor.util.Pair;
@@ -73,19 +71,14 @@ public class CriticalPairs {
      * @param r2 The second rule
      */
     private void addCriticalPairsForCollapsedRule(Pair<Nonterminal, CollapsedHeapConfiguration> r1,
-                                         Pair<Nonterminal, CollapsedHeapConfiguration> r2) {
+                                                  Pair<Nonterminal, CollapsedHeapConfiguration> r2) {
         HeapConfiguration hc1 = r1.second().getCollapsed();
         HeapConfiguration hc2 = r2.second().getCollapsed();
         if (!(hc1 instanceof Graph) || !(hc2 instanceof  Graph)) {
             throw new IllegalArgumentException("Right side of rule is not of type 'Graph'");
         }
-        Graph hc1Graph = createBipartiteGraph(hc1);
-        Graph hc2Graph = createBipartiteGraph(hc2);
-
-        TIntArrayList nodesHc1 = new TIntArrayList(hc1.countNodes());
-        TIntArrayList edgesHc1 = new TIntArrayList(hc1Graph.size() - hc1.countNodes());
-        TIntArrayList nodesHc2 = new TIntArrayList(hc2.countNodes());
-        TIntArrayList edgesHc2 = new TIntArrayList(hc2Graph.size() - hc2.countNodes());
+        Collection<GraphElement> nodesHc1, nodesHc2, edgesHc1, edgesHc2;
+        //TODO: Continue implementing
 
     }
 
@@ -96,41 +89,22 @@ public class CriticalPairs {
      * @return A pair of two TIntArrayList objects. The first contains the private ids of the nodes and the second
      * element contains the private ids of the edges
      */
-    private Pair<TIntArrayList, TIntArrayList> getNodesAndEdges(InternalHeapConfiguration hc) {
+    private Pair<TIntArrayList, TIntArrayList> getNodesAndEdges(HeapConfiguration hc) {
+        // TODO: Rewrite this to support GraphElement type
         TIntArrayList nodes = new TIntArrayList(hc.countNodes());
-        TIntArrayList edges = new TIntArrayList(hc.size() - hc.countNodes());
-        for (int node = 0; node > hc.size(); node++) {
-            NodeLabel label = hc.getNodeLabel(node);
+        TIntArrayList edges = new TIntArrayList(((Graph) hc).size() - hc.countNodes());
+        for (int node = 0; node > ((Graph)hc).size(); node++) {
+            NodeLabel label = ((Graph) hc).getNodeLabel(node);
             if (label instanceof Type) {
                 nodes.add(node);
             } else if (label instanceof Nonterminal || label instanceof SelectorLabel) {
                 // We don't include variable edges here, because they should not appear in RHS
                 edges.add(node);
             } else {
-                throw new InvalidParameterException("InternalHeapConfiguration contains unrecognized label");
+                throw new InvalidParameterException("HeapConfiguration contains unrecognized label");
             }
         }
         return new Pair<>(nodes, edges);
-    }
-
-    /**
-     * Creates a copy of an InternalHeapConfiguration where all selector edges are replaced by nonterminal edges of rank
-     * 2. These edges can be distinguished from the original nonterminal edges because the label of these selectors
-     * edges is SelectorLabelAsNonterminal. The type of the original nonterminals stays BasicNonterminal.
-     */
-    private Graph createBipartiteGraph(HeapConfiguration hc) {
-        HeapConfigurationBuilder builder = hc.clone().builder();
-        hc.nodes().forEach(node -> {
-            for (SelectorLabel label : hc.selectorLabelsOf(node)) {
-                // 1. Remove the current selector
-                builder.removeSelector(node, label);
-                // 2. Reinsert as a bipartite selector edge
-                int target = hc.selectorTargetOf(node, label);
-                builder.addSelectorBipartite(node, label, target);
-            }
-            return true;
-        });
-        return (Graph) builder.build();
     }
 
 }
