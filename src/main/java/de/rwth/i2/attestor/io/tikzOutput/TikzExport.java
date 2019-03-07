3a637af98fdc92de46a4055314627f8ba41659d4
Johannes Schulte <JohannesB.Schulte@gmail.com>
2019-03-07 15:23:52 +0100

    Using public ids in graph reports

--- src/main/java/de/rwth/i2/attestor/io/tikzOutput/TikzExport.java
+++ src/main/java/de/rwth/i2/attestor/io/tikzOutput/TikzExport.java
@@ -3,11 +3,11 @@ package de.rwth.i2.attestor.io.tikzOutput;
 import de.rwth.i2.attestor.grammar.CollapsedHeapConfiguration;
 import de.rwth.i2.attestor.grammar.Grammar;
 import de.rwth.i2.attestor.grammar.confluence.CriticalPair;
-import de.rwth.i2.attestor.grammar.confluence.jointMorphism.EdgeGraphElement;
 import de.rwth.i2.attestor.grammar.confluence.jointMorphism.JointHeapConfiguration;
 import de.rwth.i2.attestor.grammar.confluence.jointMorphism.NodeGraphElement;
 import de.rwth.i2.attestor.grammar.confluence.main.ConfluenceTool;
 import de.rwth.i2.attestor.graph.Nonterminal;
+import de.rwth.i2.attestor.graph.SelectorLabel;
 import de.rwth.i2.attestor.graph.heap.HeapConfiguration;
 import de.rwth.i2.attestor.graph.heap.internal.InternalHeapConfiguration;
 import de.rwth.i2.attestor.graph.morphism.Graph;
@@ -174,55 +174,80 @@ public class TikzExport {
      * Draws a HeapConfiguration (must already be inside a tikzpicture environment)
      */
     private void addHeapConfiguration(String pgfPath, HeapConfiguration hc, String reportStep) {
-        Graph graph = getGraph(hc);
+        addNodesAndSelectorEdges(pgfPath, hc);
+        addNonterminals(pgfPath, hc);
+    }
 
+    private void addNodesAndSelectorEdges(String pgfPath, HeapConfiguration hc) {
         Collection<String> nodes = new ArrayList<>();
-        for (NodeGraphElement nodeGraphElement : NodeGraphElement.getNodes(graph)) {
-            int privateId = nodeGraphElement.getPrivateId();
-            nodes.add(String.valueOf(privateId));
-            String currentPath = pgfPath + "/nodes/" + privateId;
-            Type nodeType = (Type) graph.getNodeLabel(nodeGraphElement.getPrivateId());
-            pgfSingleValues.add(new Pair<>(currentPath + "/type", nodeType.toString()));
-            boolean isExternal = graph.isExternal(nodeGraphElement.getPrivateId());
-            pgfSingleValues.add(new Pair<>(currentPath + "/is external", isExternal?"true":"false"));
+        hc.nodes().forEach(publicId -> {
+            Type type = hc.nodeTypeOf(publicId);
+            // Add node to pgf keys
+            nodes.add(String.valueOf(publicId));
+            String currentNodePath = pgfPath + "/nodes/" + publicId;
+            pgfSingleValues.add(new Pair<>(currentNodePath + "/type", type.toString()));
+            boolean isExternal = hc.isExternalNode(publicId);
+            pgfSingleValues.add(new Pair<>(currentNodePath + "/is external", isExternal?"true":"false"));
             if (isExternal) {
-                pgfSingleValues.add(new Pair<>(currentPath + "/external index", String.valueOf(graph.getExternalIndex(privateId))));
+                String externalIndex = String.valueOf(hc.externalIndexOf(publicId));
+                pgfSingleValues.add(new Pair<>(currentNodePath + "/external index", externalIndex));
             }
-        }
-        pgfListValues.add(new Pair<>(pgfPath+ "/nodes", nodes));
 
-
-        int selectorNumber = 0;
-        Collection<String> nonterminals = new ArrayList<>();
-        for (EdgeGraphElement currentEdge : EdgeGraphElement.getEdgesOfGraph(graph)) {
-            if (currentEdge.isSelector()) {
-                String currentPath = pgfPath + "/selectors/" + selectorNumber;
-                pgfSingleValues.add(new Pair<>(currentPath + "/from", String.valueOf(currentEdge.getPrivateId())));
-                String selectorDestination = String.valueOf(currentEdge.getConnectedNodes(graph).get(1).getPrivateId());
-                pgfSingleValues.add(new Pair<>(currentPath + "/to", selectorDestination));
-                pgfSingleValues.add(new Pair<>(currentPath + "/label", currentEdge.getSelectorLabel()));
-                selectorNumber++;
-            } else {
-                // Nonterminal edge TODO: set "isNew" argument correctly
-                int privateId = currentEdge.getPrivateId();
-                String currentPath = pgfPath + "/nonterminals/" + privateId;
-                nonterminals.add(String.valueOf(privateId));
-                int rank = graph.getSuccessorsOf(privateId).size();
-                pgfSingleValues.add(new Pair<>(currentPath + "/rank", String.valueOf(rank)));
-                Nonterminal nonterminal = (Nonterminal) graph.getNodeLabel(privateId);
-                pgfSingleValues.add(new Pair<>(currentPath + "/label", nonterminal.getLabel()));
-                TIntArrayList successors = graph.getSuccessorsOf(privateId);
-                for (int i=0; i<rank; i++) {
-                    String successor = String.valueOf(successors.get(i));
-                    pgfSingleValues.add(new Pair<>(currentPath + "/tentacle/" + i, successor));
+            // Find all selector edges (Restructure them for easier processing)
+            Map<Integer, Collection<SelectorLabel>> targetToSelectorsMap = new HashMap<>();
+            for (SelectorLabel selectorLabel : hc.selectorLabelsOf(publicId)) {
+                int selectorTarget = hc.selectorTargetOf(publicId, selectorLabel);
+                Collection<SelectorLabel> selectorsOfTarget;
+                if (targetToSelectorsMap.containsKey(selectorTarget)) {
+                    selectorsOfTarget = targetToSelectorsMap.get(selectorTarget);
+                } else {
+                    selectorsOfTarget = new ArrayList<>();
+                    targetToSelectorsMap.put(selectorTarget, selectorsOfTarget);
                 }
+                selectorsOfTarget.add(selectorLabel);
             }
-        }
-        pgfSingleValues.add(new Pair<>(pgfPath + "/num selectors", String.valueOf(selectorNumber)));
+            // Add the neccessary keys for the selector edges
+            Collection<String> selectorTargets = new ArrayList<>();
+            for (Integer target : targetToSelectorsMap.keySet()) {
+                selectorTargets.add(String.valueOf(target));
+                Collection<String> selectorLabels = new ArrayList<>();
+                for (SelectorLabel label : targetToSelectorsMap.get(target)) {
+                    selectorLabels.add(label.getLabel());
+                }
+                pgfListValues.add(new Pair<>(currentNodePath + "/selector/" + target, selectorLabels));
+            }
+            pgfListValues.add(new Pair<>(currentNodePath + "/selector targets", selectorTargets));
+            // Continue with the other nodes
+            return true;
+        });
+        pgfListValues.add(new Pair<>(pgfPath + "/nodes", nodes));
+    }
+
+    private void addNonterminals(String pgfPath, HeapConfiguration hc) {
+        Collection<String> nonterminals = new ArrayList<>();
+        hc.nonterminalEdges().forEach(publicId -> {
+            Nonterminal nonterminal = hc.labelOf(publicId);
+            int rank = nonterminal.getRank();
+            // Add nonterminal to pgf keys
+            nonterminals.add(String.valueOf(publicId));
+            String currentNodePath = pgfPath + "/nonterminals/" + publicId;
+            pgfSingleValues.add(new Pair<>(currentNodePath + "/label", nonterminal.getLabel()));
+            pgfSingleValues.add(new Pair<>(currentNodePath + "/rank", String.valueOf(rank)));
+            // Add tentacle edges
+            TIntArrayList attachedNodes = hc.attachedNodesOf(publicId);
+            for (int i=0; i<rank; i++) {
+                String tentacleDestination = String.valueOf(attachedNodes.get(i));
+                pgfSingleValues.add(new Pair<>(currentNodePath + "/tentacles/" + i, tentacleDestination));
+            }
+            return true;
+        });
         pgfListValues.add(new Pair<>(pgfPath + "/nonterminals", nonterminals));
     }
 
+
+
     private void writeCurrentReportToFile(String reportCommand) throws IOException {
+        // TODO: Escape characters (allow only spaces and A-Za-z0-9 and in paths allow "/")
         String indent = "    ";
         String lineSeparator = "%" + System.lineSeparator();
         StringBuilder result = new StringBuilder();
