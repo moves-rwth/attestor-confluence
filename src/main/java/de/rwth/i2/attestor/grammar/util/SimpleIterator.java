963695d9ba649df532f60ddd7120989419961e0c
Johannes Schulte <mail@j-schulte.com>
2019-04-09 18:31:41 +0200

    Fixed issue with SimpleIterator

--- src/main/java/de/rwth/i2/attestor/grammar/util/SimpleIterator.java
+++ src/main/java/de/rwth/i2/attestor/grammar/util/SimpleIterator.java
@@ -6,21 +6,27 @@ import java.util.Iterator;
  * A shortcut iterator that only requries a single method to be implemented. Cannot return null.
  */
 public abstract class SimpleIterator<T> implements Iterator<T> {
-    private T nextElement;
+    private T nextElement = null;
+    boolean firstCall = true;
 
     public abstract T computeNext();
 
-    public SimpleIterator() {
-        nextElement = computeNext();
+    private void checkFirstCall() {
+        if (firstCall) {
+            nextElement = computeNext();
+            firstCall = false;
+        }
     }
 
     @Override
     public boolean hasNext() {
+        checkFirstCall();
         return nextElement != null;
     }
 
     @Override
     public T next() {
+        checkFirstCall();
         T temp = nextElement;
         nextElement = computeNext();
         return temp;
