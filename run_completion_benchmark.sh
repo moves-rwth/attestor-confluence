3221776131cee525549b08397e4e8734d4b28fd7
Johannes Schulte <mail@j-schulte.com>
2019-05-15 14:06:05 +0200

    Updated completion benchmark runner script

--- run_completion_benchmark.sh
+++ run_completion_benchmark.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
-mvn compile
-time mvn exec:java -Dexec.mainClass="de.rwth.i2.attestor.grammar.confluence.benchmark.CompletionBenchmarkRunner"
+NUM_CORES=$(nproc)
+NUM_THREADS=$@
+
+#mvn compile
+
+for i in {1..100}
+do
+	while true
+	do
+		RUNNING_PROCESSES=$( grep procs_running /proc/stat | cut -d' ' -f2- )
+		if test $RUNNING_PROCESSES -lt $(( $NUM_CORES - $NUM_THREADS ))
+		then
+			break
+		fi
+		sleep 10
+	done
+	echo "Starting benchmark run $i"
+	( (time mvn exec:java -Dexec.mainClass="de.rwth.i2.attestor.grammar.confluence.benchmark.CompletionBenchmarkRunner" -Dexec.args="$NUM_THREADS") > log_${i}.txt 2>&1 )&
+	sleep 10
+done
 
